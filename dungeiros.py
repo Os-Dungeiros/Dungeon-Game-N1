5d8a90b9f5d2346ff1040d30dad22de186755acf
Ruan Lima Ribeiro <devd8727@gmail.com>
2021-09-15 09:33:49 -0300

    Update dungeiros.py

--- dungeiros.py
+++ dungeiros.py
@@ -1,4 +1,9 @@
 
+numeroSala = 1
+escolhaCaminho = 0
+cont = 0
+print("\nOlá aventureiro!! :D\n")
+print("--------------------------\nSala atual (inicial) : {}\n--------------------------".format(numeroSala))
 while numeroSala != 9 and cont < 7:
     escolhaCaminho = int(input("Qual caminho você seguirá (se escolher algum diferente de 1 ou 2 será adicionado 1)?\n[1] Vermelho\n[2] Preto\nEscolha : "))
     
