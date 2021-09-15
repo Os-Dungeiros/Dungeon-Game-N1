685f2d860b15b1b2fb046e1be5221f9240bc6f28
Daniel Camargo <88405313+DanielRubioCamargo@users.noreply.github.com>
2021-09-15 09:25:47 -0300

    Loop while, comando de entrada para a escolha de caminho e incremento no contador

--- dungeiros.py
+++ dungeiros.py
@@ -1,6 +1,6 @@
 
-numeroSala = 1
-escolhaCaminho = 0
-cont = 0
-print("\nOlá aventureiro!! :D\n")
-print("--------------------------\nSala atual (inicial) : {}\n--------------------------".format(numeroSala))
+while numeroSala != 9 and cont < 7:
+    escolhaCaminho = int(input("Qual caminho você seguirá (se escolher algum diferente de 1 ou 2 será adicionado 1)?\n[1] Vermelho\n[2] Preto\nEscolha : "))
+    
+    print("----------------------\nSua sala atual é : {}\n----------------------\n".format(numeroSala))
+    cont = cont + 1
